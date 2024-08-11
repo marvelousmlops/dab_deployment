6f1c73fd3ac560ab0ed6fd4e9699afef30f2e204
Maria Vechtomova <130861802+mvechtomova@users.noreply.github.com>
2024-08-11 13:23:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from my_package.example import hello_you
-from loguru import logger
-from adplogger.utils import configure_adplogger, ADPLoggerConfig
 import yaml
 import argparse
 
@@ -20,19 +18,3 @@ if __name__ == '__main__':
     with open(f"/Workspace/{root_path}/files/name.yml", "r") as file:
         config = yaml.safe_load(file)
     hello_you(config['name'])
-    
-
-    logger_config = ADPLoggerConfig(
-
-        # The default extra fields to be added to every log record. Those are examples.
-        default_extra={
-            "env": "dev",
-            "opco": "cz", #To be added
-            "data_product": "mydataproduct",
-            "adf_pipeline": "aggregate_daily",
-            "adf_run_uuid": "00000000-0000-0000-0000-000000000000"},
-    )
-
-    configure_adplogger(logger_config)
-
-    logger.info("Log message")
